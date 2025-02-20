164b573939a515ae5142222f67012e08d51f8561
Dhanraj30 <dhanraj30102002@gmail.com>
2025-02-20 22:59:48 +0530

    auth integration and updates

---
+++ src/app/(protected)/app-sidebar.tsx
@@ -0,0 +1,135 @@
+'use client'
+
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarHeader, SidebarMenu, SidebarGroupLabel, SidebarMenuButton, SidebarMenuItem, useSidebar } from '@/components/ui/sidebar'
+import { Bot, LayoutDashboard, Presentation, CreditCard, Plus } from 'lucide-react'
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+
+import { usePathname } from 'next/navigation';
+import { Button } from 'react-day-picker';
+
+import Image from 'next/image';
+
+const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Q&A",
+    url: "/qa",
+    icon: Bot,
+  },
+  {
+    title: "Meetings",
+    url: "/meetings",
+    icon: Presentation,
+  },
+  {
+    title: "Billing",
+    url: "/billing",
+    icon: CreditCard,
+  }
+];
+
+const projects = [
+    {
+        name: 'Project 1',
+    },
+    {
+        name: 'Project 2',
+    },
+    {
+        name: 'Project 3',
+    },
+    
+]
+
+export function AppSidebar() {
+    const pathname = usePathname();
+    const { open } = useSidebar();
+  return (
+    <Sidebar collapsible="icon" variant="floating">
+        <SidebarHeader>
+            <div className='flex items-center gap-2'>
+                <Image src='/logo.png' alt='logo' width={40} height={40} />
+                {open && (
+                    <span className='text-lg font-bold text-primary/80'>Gensis</span>
+                )}
+            </div>
+        </SidebarHeader>
+    <SidebarContent>
+    <SidebarGroup>
+      <SidebarGroupLabel>
+        Application
+      </SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+        {items.map((item) => {
+          return (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton asChild>
+                <Link 
+                  href={item.url} 
+                  className={cn({
+                    '!bg-primary !text-white': pathname === item.url
+                }, 'list-none')}
+                >
+                  <item.icon />
+                  <span>{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          );
+        })}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+
+    <SidebarGroup>
+  <SidebarGroupLabel>
+    Your Projects
+  </SidebarGroupLabel>
+  <SidebarGroupContent>
+    <SidebarMenu>
+      {projects.map(project => {
+        return (
+          <SidebarMenuItem key={project.name}>
+            <SidebarMenuButton asChild>
+              <div>
+                <div className={cn(
+                    'rounded-sm border size-6 flex items-center justify-center text-sm bg-white text-primary',
+                    {
+                        'bg-primary text-white': true 
+                    })}>
+                  {project.name[0]}
+                </div>
+                <span>{project.name}</span>
+              </div>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        );
+      })}
+      <div className='h-2'></div>
+
+      {open && (
+      <SidebarMenuItem>
+            <Link href='/create'>
+                <Button size='sm' variant={'outline'} className='w-fit'>
+                    <Plus />
+                    Create Project
+                </Button>
+            </Link>      
+      </SidebarMenuItem>
+      )}
+      
+    </SidebarMenu>
+  </SidebarGroupContent>
+</SidebarGroup>
+
+  </SidebarContent>
+</Sidebar>
+
+  );
+}
